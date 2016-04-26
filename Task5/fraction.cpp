c1baa7a38e5a472378e7286b367f599a21963bcf
Reece Volker <reecevolker@gmail.com>
2016-04-27 00:07:35 +0200

    tar files for first atemp

---
+++ Task5/fraction.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+
+#include "reduce.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Fraction Reduction" << endl;
+    cout << "-------------------------------" << endl;
+
+    int a, b;
+
+    cout << "Enter the numerator: ";
+    cin >> a;
+
+    cout << "Enter the denominator: ";
+    cin >> b;
+
+    int absA, absB; // Get absolute values of variables
+    absA = abs(a);
+    absB = abs(b);
+
+    cout << "The GCD of " << absA << " and " << absB << " is " << gcd( absA, absB ) << endl;
+
+    cout << "The lowest form of ";
+    displayFraction( a, b );
+    cout << " is ";
+
+    if( ( a * b ) < 0 ) // it's positive
+        cout << "-";
+
+    reduceFraction( absA, absB );
+    displayFraction( absA, absB );
+
+    cout << " = " << static_cast<double>(a)/b << endl;
+
+    return 0;
+}
+
